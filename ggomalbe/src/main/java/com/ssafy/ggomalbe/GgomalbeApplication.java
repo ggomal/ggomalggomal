e7d113d25efe0081b6046ca17ce61fcc1e552aa1
changhi97 <77597957+changhi97@users.noreply.github.com>
2024-04-22 02:10:40 +0900

    test bakc-end

---
+++ ggomalbe/src/main/java/com/ssafy/ggomalbe/GgomalbeApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ggomalbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgomalbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgomalbeApplication.class, args);
+	}
+
+}

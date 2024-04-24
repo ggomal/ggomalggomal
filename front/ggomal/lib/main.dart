8c6a9c9cdc55f0f859021f52b1140c65e7804607
지민 <81950388+jjm6604@users.noreply.github.com>
2024-04-24 09:39:09 +0900

    Feat : login background animation
    Change-Id: Ib5b9241570294436723bcaea15aa0e45053c7470

--- front/ggomal/lib/main.dart
+++ front/ggomal/lib/main.dart
@@ -4,46 +4,72 @@ import 'package:animated_background/animated_background.dart';
 // 메인
 void main() {
   runApp(
-    MaterialApp(home: MainScreen()),
+    MaterialApp(home: LoginScreen()),
   );
 }
 
-  
-class MainScreen extends StatefulWidget {
-  const MainScreen({Key? key}) : super(key: key);
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
   @override
-  State<MainScreen> createState() => _MainScreen();
+  State<LoginScreen> createState() => _LoginScreen();
 }
 
-class _MainScreen extends State<MainScreen>
-    with SingleTickerProviderStateMixin {
-  ParticleOptions particles = const ParticleOptions(
-    baseColor: Colors.cyan,
-    spawnOpacity: 0.0,
-    opacityChangeRate: 0.25,
-    minOpacity: 0.1,
-    maxOpacity: 0.4,
-    particleCount: 30,
-    spawnMaxRadius: 25.0,
-    spawnMaxSpeed: 30.0,
-    spawnMinSpeed: 10,
-    spawnMinRadius: 10.0,
-  );
+class _LoginScreen extends State<LoginScreen> with TickerProviderStateMixin {
+  final List<ParticleOptionsData> particleOptionsList = [
+    ParticleOptionsData(baseColor: Colors.cyan),
+    ParticleOptionsData(baseColor: Colors.yellow),
+    ParticleOptionsData(baseColor: Colors.pink),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFEEF8FF),
-      body:
-      AnimatedBackground(
-        vsync: this,
-        behaviour: RandomParticleBehaviour(options: particles),
-        child: Center(
-          child: Image.asset(
-            'assets/images/logo.png',
+      body: Stack(
+        children: [
+          ...particleOptionsList
+              .map((data) => buildAnimatedBackground(data.particleOptions))
+              .toList(),
+          Container(
+            child: Center(
+              child: Image.asset(
+                'assets/images/logo.png',
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
+
+  Widget buildAnimatedBackground(ParticleOptions particleOptions) {
+    return Container(
+      child: AnimatedBackground(
+        vsync: this,
+        behaviour: RandomParticleBehaviour(options: particleOptions),
+        child: Container(),
+      ),
+    );
+  }
+}
+
+// particle 옵션, color만 받아서 추가
+class ParticleOptionsData {
+  final Color baseColor;
+
+  const ParticleOptionsData({required this.baseColor});
+
+  ParticleOptions get particleOptions => ParticleOptions(
+        baseColor: baseColor,
+        spawnOpacity: 0.0,
+        opacityChangeRate: 0.25,
+        minOpacity: 0.1,
+        maxOpacity: 0.4,
+        particleCount: 10,
+        spawnMaxRadius: 25.0,
+        spawnMaxSpeed: 30.0,
+        spawnMinSpeed: 10,
+        spawnMinRadius: 10.0,
+      );
 }

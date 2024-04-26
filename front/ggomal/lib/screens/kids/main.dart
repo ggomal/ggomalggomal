6ca3f2528789e10026242e381a4239c784ab7f39
박서현 <mentos0601@naver.com>
2024-04-26 17:51:29 +0900

    Feat : 게임소개모달창 수정
    Change-Id: I96e2b2462010b9eb3b97e7d5aeaf0dae98f3f763

--- front/ggomal/lib/screens/kids/main.dart
+++ front/ggomal/lib/screens/kids/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ggomal/screens/kids/frog.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ggomal/utils/navbar.dart';
 
@@ -123,7 +125,6 @@ class GameDialogUI extends StatelessWidget {
     return Dialog(
       backgroundColor: Colors.transparent,
       child: Container(
-        // padding: EdgeInsets.all(20),
         width: width,
         height: height,
         decoration: BoxDecoration(
@@ -134,34 +135,125 @@ class GameDialogUI extends StatelessWidget {
         child: Column(
           children: [
             Flexible(
-              flex: 3,
+                flex: 9,
                 child: Stack(
                   children: [
-                        Expanded(
-                          child: Container(
-                            alignment: Alignment.center,
-                            child: Text("개구리 게임"),
-                          ),
+                    Expanded( child: LayoutBuilder(
+                        builder: (context, constraints) {
+                        return Container(
+                        alignment: Alignment.center,
+                        child: Container(
+                            height: height * 0.15,
+                            padding: EdgeInsets.symmetric(
+                              horizontal: constraints.maxHeight * 0.4,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Color(0xFFFAE0CA),
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey,
+                                  spreadRadius: 2,
+                                  blurRadius: 2,
+                                  offset: Offset(
+                                      0, 3), // changes position of shadow
+                                )
+                              ],
+                            ),
+                            child: FittedBox(
+                              fit: BoxFit.fill,
+                              child: Text(
+                                "개구리 게임",
+                                style: TextStyle(fontFamily: 'MapleStoryBold'),
+                              ),
+                            )),
+                      );}),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: height * 0.1),
+                      alignment: Alignment.centerRight,
+                      child: Container(
+                        // margin: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                            color: Color(0xFF39855E), shape: BoxShape.circle),
+                        child: IconButton(
+                          iconSize: height * 0.1,
+                          icon: Icon(Icons.close, color: Colors.white),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
                         ),
-                        Container(
-                          decoration: BoxDecoration(
-                              color: Colors.green,
-                              shape: BoxShape.circle
-                          ),
-                          child: IconButton(
-                            icon: Icon(Icons.close, color: Colors.white),
-                            onPressed: (){},
-                          ),
-                        )
-
-
+                      ),
+                    ),
                   ],
                 )),
             Flexible(
-              flex: 8,
+                flex: 20,
                 child: Container(
-              color: Colors.blue,
-            )),
+                  color: Colors.transparent,
+                  child: Row(
+                    children: [
+                      Flexible(flex: 1, child: Container()),
+                      Flexible(flex: 12, child: Container(child:
+                        LayoutBuilder(
+                          builder: (context, constraints) {
+                            return Container(
+                              color: Colors.transparent,
+                              margin: EdgeInsets.symmetric(
+                                horizontal: constraints.maxWidth * 0.1,
+                                vertical: constraints.maxHeight * 0.1
+                              ),
+                              child: Container(color: Colors.yellow, child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Flexible(flex : 5, child : Container(child : Image.asset("assets/images/frog/mini_frog.png"))),
+                                  Flexible(flex : 5, child : Container( child: Column(
+                                    children: [
+                                      Expanded(flex: 7, child: Container(color: Colors.orange,)),
+                                      Expanded(flex: 3, child: Container(
+                                        child: LayoutBuilder(
+                                          builder: (context, constraints){
+                                            double buttonPading = constraints.maxWidth * 0.05;
+                                            return Container(
+                                              child: Center(
+                                                child: ElevatedButton(
+                                                  onPressed: (){
+
+                                                  },
+                                                  child: Padding(
+                                                    padding: EdgeInsets.symmetric(horizontal: 20),
+                                                    child: Text(
+                                                      "시작하기",
+                                                      style: TextStyle(
+                                                        fontSize: constraints.maxWidth * 0.05, fontFamily: 'MapleStoryBold', color: Color(0xFF003D06)
+                                                      ),
+                                                    ),
+                                                  ),
+                                                  style: ElevatedButton.styleFrom(
+                                                    backgroundColor: Color(0xFFCFE4D1),
+                                                    shape: RoundedRectangleBorder(
+                                                      borderRadius: BorderRadius.circular(12),
+                                                      side: BorderSide(color: Color(0xFF455747), width: 2.0)
+                                                    )
+                                                  ),
+                                                ),
+                                              ),
+                                            );
+                                          },
+                                        ),
+                                      ))
+                                    ],
+                                  ),)),
+                                ],
+                              )),
+                            );
+                          },
+                        ),)),
+                      Flexible(flex: 1, child: Container(color: Colors.red,)),
+                    ],
+                  ),
+                )),
+            Flexible(flex:4 , child: Container(),)
           ],
         ),
       ),

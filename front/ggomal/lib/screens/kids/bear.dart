e0c04d4488b01a2d5d4bb633ff43575080d72b22
subin22643 <139415947+subin22643@users.noreply.github.com>
2024-04-26 17:31:22 +0900

    Feat : 곰 집 추가
    Change-Id: Ie11e27aeeff7e32c00880c55aeff95426ea1b0e0

--- front/ggomal/lib/screens/kids/bear.dart
+++ front/ggomal/lib/screens/kids/bear.dart
@@ -14,9 +14,64 @@ class BearScreen extends StatelessWidget {
         ),
       ),
       child: Scaffold(
-          backgroundColor: Colors.transparent,
-          appBar: NavBar(),
-          body: Text('곰집입니다')),
+        backgroundColor: Colors.transparent,
+        appBar: NavBar(),
+        body: Stack(
+          children: [
+            Column(
+              children: [
+                Flexible(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Image.asset('assets/images/bear/window.png'),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Image.asset('assets/images/bear/mirror.png'),
+                      ),
+                    ],
+                  ),
+                ),
+                Flexible(
+                  flex: 3,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Image.asset('assets/images/bear/chair1.png'),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Image.asset('assets/images/bear/chair2.png'),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: SizedBox(child: Image.asset('assets/images/bear/teacher.png'), width: 260,),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Positioned(
+              top: 300, // 조정 가능, chair1과 chair2의 사이 중앙
+              left: 160, // 조정 가능
+              child: InkWell(
+                onTap: () {},
+                child: SizedBox(
+                  width: 600,
+                  child: Image.asset('assets/images/bear/table.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

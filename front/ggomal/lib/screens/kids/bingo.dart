c2f9ef94e3ad7ae561b70f29c69f2609337d2f22
subin22643 <139415947+subin22643@users.noreply.github.com>
2024-04-29 16:34:06 +0900

    Feat : 빙고 소켓 초기 셋팅
    Change-Id: Ie554f3d8929fb30d7e3d8a074c29cab496417e4a

--- front/ggomal/lib/screens/kids/bingo.dart
+++ front/ggomal/lib/screens/kids/bingo.dart
@@ -1,30 +1,61 @@
-// import 'package:socket_io_client/socket_io_client.dart' as IO;
-//
-// main() {
-//   // Dart client
-//   IO.Socket socket = IO.io('http://localhost:3000');
-//   socket.onConnect((_) {
-//     print('connect');
-//     socket.emit('msg', 'test');
-//   });
-//   socket.on('event', (data) => print(data));
-//   socket.onDisconnect((_) => print('disconnect'));
-//   socket.on('fromServer', (_) => print(_));
-// }
-
 import 'package:flutter/material.dart';
 import 'package:ggomal/utils/navbar.dart';
+// import 'package:web_socket_channel/web_socket_channel.dart';
+// import 'package:web_socket_channel/status.dart' as status;
 
-class BingoScreen extends StatelessWidget {
+class BingoScreen extends StatefulWidget {
   const BingoScreen({super.key});
 
   @override
+  State<BingoScreen> createState() => _BingoScreenState();
+}
+
+class _BingoScreenState extends State<BingoScreen> {
+  // final socketUrl = Uri.parse('wss://k10e206.p.ssafy.io/api/v1/room');
+  // final channel = WebSocketChannel.connect(Uri.parse('wss://k10e206.p.ssafy.io/api/v1/room'));
+  //
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   connectToWebSocket();
+  // }
+  //
+  // void connectToWebSocket() {
+  //   try {
+  //     channel.stream.listen((message) {
+  //       print("소켓 통신 성공~~~!: $message");
+  //       // channel.sink.add('소켓 통신~~~!!!');
+  //     }, onDone: () {
+  //       print('소켓 통신 성공 했다가 닫힐때 나오는 메시지임 ');
+  //     }, onError: (error) {
+  //       print('소켓 통신에 실패했습니다.');
+  //     });
+  //     print('WebSocket connection established.');
+  //   } catch (e) {
+  //     print('소켓 통신 에러날 때 나오는 메시지 $e');
+  //   }
+  // }
+  //
+  // @override
+  // void dispose() {
+  //   channel.sink.close(status.goingAway);
+  //   super.dispose();
+  // }
+
+  @override
   Widget build(BuildContext context) {
+    // channel.ready;
+    //
+    // channel.stream.listen((message) {
+    //   channel.sink.add('소켓 통신~~~!!!');
+    //   channel.sink.close(status.goingAway);
+    // });
+
     return Container(
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/bear/bingo_bg.png"),
-            fit: BoxFit.cover, // 화면에 꽉 차게
+            fit: BoxFit.cover,
           ),
         ),
         child: Scaffold(
@@ -35,41 +66,44 @@ class BingoScreen extends StatelessWidget {
                 child: Row(
               children: [
                 Flexible(
-                  child: Container(
-                  ),
+                  child: Container(),
                   flex: 1,
                 ),
                 Flexible(
-                  child: Column(children: [
-                    Flexible(
-                      child: Center(
-                        child: Container(
-                          width: 200,
-                          height: 100,
-                          decoration: BoxDecoration(
-                            color: Color(0xffcfe4d1),
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border.all(
-                              color: Colors.black,
-                              width: 4,
+                  child: Column(
+                    children: [
+                      Flexible(
+                        child: Center(
+                          child: Container(
+                            width: 200,
+                            height: 90,
+                            decoration: BoxDecoration(
+                              color: Color(0xffcfe4d1),
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 4,
+                              ),
                             ),
-                          ),
-                          alignment: Alignment.center,
-                          child: Text(
-                            '아이 이름',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 30,
+                            alignment: Alignment.center,
+                            child: Text(
+                              '이안이',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontFamily: 'Maplestory',
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    Flexible(
-                      child: Container(color: Colors.red),
-                      flex: 4,
-                    ),
-                  ],),
+                      Flexible(
+                        child: Container(color: Colors.red),
+                        flex: 4,
+                      ),
+                    ],
+                  ),
                   flex: 3,
                 ),
                 Flexible(
@@ -78,7 +112,7 @@ class BingoScreen extends StatelessWidget {
                       child: Center(
                         child: Container(
                           width: 200,
-                          height: 100,
+                          height: 90,
                           decoration: BoxDecoration(
                             color: Color(0xffcfe4d1),
                             borderRadius: BorderRadius.circular(50),
@@ -92,7 +126,9 @@ class BingoScreen extends StatelessWidget {
                             '선생님',
                             style: TextStyle(
                               color: Colors.black,
-                              fontSize: 30,
+                              fontFamily: 'Maplestory',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40,
                             ),
                           ),
                         ),
@@ -106,30 +142,30 @@ class BingoScreen extends StatelessWidget {
                   flex: 3,
                 ),
                 Flexible(
-                  child: Container(
-                  ),
+                  child: Container(),
                   flex: 1,
                 )
               ],
-            ))
-          , Positioned(
-              left: 0, // 왼쪽 가장자리
-              bottom: 0, // 아래쪽 가장자리
+            )),
+            Positioned(
+              left: 0,
+              bottom: 0,
               child: Image.asset(
-                'assets/images/bear/student.png', // 왼쪽 아래 이미지
-                width: 250, // 너비 설정
-                height: 250, // 높이 설정
+                'assets/images/bear/student.png',
+                width: 250,
+                height: 250,
               ),
             ),
             Positioned(
-              right: 0, // 오른쪽 가장자리
-              bottom: 5, // 아래쪽 가장자리
+              right: 0,
+              bottom: 5,
               child: Image.asset(
-                'assets/images/bear/teacher.png', // 오른쪽 아래 이미지
-                width: 250, // 너비 설정
-                height: 250, // 높이 설정
+                'assets/images/bear/teacher.png',
+                width: 250,
+                height: 250,
               ),
-            ),]),
+            ),
+          ]),
         ));
   }
 }

bd9353352ba491839981e00136f62b6efbf589a9
지민 <81950388+jjm6604@users.noreply.github.com>
2024-04-29 12:08:30 +0900

    Feat : kids_manage 구성
    Change-Id: Ic6f2c68c88e48e39e09faac29aaf6d30557fc517

---
+++ front/ggomal/lib/screens/manager/kid_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KidDetail extends StatelessWidget {
+  const KidDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

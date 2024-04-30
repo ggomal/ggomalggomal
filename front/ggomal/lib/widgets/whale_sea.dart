bcf2601cdf6d791bd5dae8141ae836ebd0d5747f
지민 <81950388+jjm6604@users.noreply.github.com>
2024-04-30 11:12:18 +0900

    Feat : whale diver move
    Change-Id: I64ffdab862b61a97864e4e160bcc6c4f81d9e493

--- front/ggomal/lib/widgets/whale_sea.dart
+++ front/ggomal/lib/widgets/whale_sea.dart
@@ -10,13 +10,21 @@ class WhaleSea extends StatefulWidget {
 
 class _WhaleSeaState extends State<WhaleSea> {
   Alignment _alignment = Alignment.center;
+
   late Timer _timer;
 
   void _startMove(List<double> direction) {
     _timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
-      setState(() {
-          _alignment += Alignment(direction[0], direction[1]);
-      });
+      setState(
+        () {
+          double x = _alignment.x + direction[0];
+          double y = _alignment.y + direction[1];
+
+          if (x >= -1 && x <= 1 && y >= -1 && y <= 1) {
+            _alignment += Alignment(direction[0], direction[1]);
+          }
+        },
+      );
     });
   }
 
@@ -26,52 +34,64 @@ class _WhaleSeaState extends State<WhaleSea> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 150,
-      ),
-      child: GestureDetector(
-        onPanCancel: () => _stopMove(),
-        child: Stack(
-          children: [
-            Image.asset(
-              'assets/images/whale/whale_sea.png',
+    return GestureDetector(
+      onPanCancel: () => _stopMove(),
+      child: Stack(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            alignment: _alignment,
+            child: Image.asset(
+              'assets/images/whale/whale_diver.png',
+              width: 200.0,
             ),
-            AnimatedContainer(
-              duration: Duration(milliseconds: 200),
-              curve: Curves.easeInOut,
-              alignment: _alignment,
-              child: Image.asset(
-                'assets/images/whale/whale_character.png',
-                width: 200.0,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                [0.0, 0.1],
-                [0.1, 0.0],
-                [0.0, -0.1],
-                [-0.1, 0.0]
-              ]
-                  .map(
-                    (e) => GestureDetector(
-                      onTapDown: (_) => _startMove(e),
+          ),
+          Stack(
+            children: [
+              {
+                "direct": "top",
+                "coord": [0.0, -0.1],
+                "location": Alignment.topCenter,
+                "word": "아"
+              },
+              {
+                "direct": "bottom",
+                "coord": [0.0, 0.1],
+                "location": Alignment.bottomCenter,
+                "word": "에"
+              },
+              {
+                "direct": "left",
+                "coord": [-0.07, 0.0],
+                "location": Alignment.centerLeft,
+                "word": "이"
+              },
+              {
+                "direct": "right",
+                "coord": [0.07, 0.0],
+                "location": Alignment.centerRight,
+                "word": "오"
+              },
+            ]
+                .map(
+                  (e) => Align(
+                    alignment: e['location'] as Alignment,
+                    child: GestureDetector(
+                      onTapDown: (_) => _startMove(e['coord'] as List<double>),
                       onTapUp: (_) => _stopMove(),
                       onTapCancel: () => _stopMove(),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: Text('$e 방향'),
+                      child: Image.asset(
+                        "assets/images/whale/button_${e['direct']}.png",
+                        width: 130,
                       ),
                     ),
-                  )
-                  .toList(),
-            ),
-          ],
-        ),
+                  ),
+                )
+                .toList(),
+          ),
+        ],
       ),
     );
   }
-
-
 }

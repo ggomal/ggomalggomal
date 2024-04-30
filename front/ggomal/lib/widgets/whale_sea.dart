aa447474f8c757f9dcc9570d91541f4e797074fe
지민 <81950388+jjm6604@users.noreply.github.com>
2024-04-30 15:42:03 +0900

    Feat : whale eat fish
    Change-Id: I0e6a5c636fa0f55e6a90ef2d9cd79bdb162cd95f

--- front/ggomal/lib/widgets/whale_sea.dart
+++ front/ggomal/lib/widgets/whale_sea.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
+
+import 'package:ggomal/widgets/percent_bar.dart';
 
 class WhaleSea extends StatefulWidget {
   const WhaleSea({super.key});
@@ -13,15 +16,35 @@ class _WhaleSeaState extends State<WhaleSea> {
 
   late Timer _timer;
 
+  late int _fishCount = 0;
+
+  final List<Map<String, dynamic>> _fishLocation = [
+    {"x": -0.1, "y" : 0.6, "isVisible": true},
+    {"x": -0.3, "y" : 0.2, "isVisible": true},
+    {"x": 0.5, "y" : 0.25, "isVisible": true},
+    {"x": -0.7, "y" : 0.4, "isVisible": true},
+    {"x": 0.7, "y" : -0.5, "isVisible": true},
+    {"x": -0.46, "y" : -0.2, "isVisible": true},
+    {"x": 0.12, "y" : -0.6, "isVisible": true},
+    {"x": 0.6, "y" : -0.4, "isVisible": true},
+    {"x": -0.22, "y" : 0.28, "isVisible": true},
+    {"x": 0.4, "y" : 0.22, "isVisible": true},
+  ];
+
+
   void _startMove(List<double> direction) {
     _timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
       setState(
         () {
           double x = _alignment.x + direction[0];
           double y = _alignment.y + direction[1];
-
           if (x >= -1 && x <= 1 && y >= -1 && y <= 1) {
             _alignment += Alignment(direction[0], direction[1]);
+            int fishIndex = _fishLocation.indexWhere((location) => x + 0.12 > location["x"]! && x - 0.12 < location["x"]! && y + 0.12 > location["y"]! && y - 0.12 < location["y"]!);
+
+            if (fishIndex != -1 && _fishLocation[fishIndex]['isVisible']){
+              _eatFish(fishIndex);
+            }
           }
         },
       );
@@ -30,6 +53,15 @@ class _WhaleSeaState extends State<WhaleSea> {
 
   void _stopMove() {
     _timer.cancel();
+  }
+
+  void _eatFish(int fishIndex) {
+    setState(
+        (){
+          _fishCount += 1;
+          _fishLocation[fishIndex]['isVisible'] = false;
+        }
+    );
   }
 
   @override
@@ -47,48 +79,62 @@ class _WhaleSeaState extends State<WhaleSea> {
               width: 200.0,
             ),
           ),
-          Stack(
-            children: [
-              {
-                "direct": "top",
-                "coord": [0.0, -0.1],
-                "location": Alignment.topCenter,
-                "word": "아"
-              },
-              {
-                "direct": "bottom",
-                "coord": [0.0, 0.1],
-                "location": Alignment.bottomCenter,
-                "word": "에"
-              },
-              {
-                "direct": "left",
-                "coord": [-0.07, 0.0],
-                "location": Alignment.centerLeft,
-                "word": "이"
-              },
-              {
-                "direct": "right",
-                "coord": [0.07, 0.0],
-                "location": Alignment.centerRight,
-                "word": "오"
-              },
-            ]
-                .map(
-                  (e) => Align(
-                    alignment: e['location'] as Alignment,
-                    child: GestureDetector(
-                      onTapDown: (_) => _startMove(e['coord'] as List<double>),
-                      onTapUp: (_) => _stopMove(),
-                      onTapCancel: () => _stopMove(),
-                      child: Image.asset(
-                        "assets/images/whale/button_${e['direct']}.png",
-                        width: 130,
-                      ),
+          ..._fishLocation.map(
+          (e) =>
+          e['isVisible'] ?
+          Align(
+            alignment: Alignment(e['x'] as double, e['y'] as double),
+            child: Image.asset(
+              "assets/images/whale/fish_pink.png",
+              width: 50,
+            ),
+          ) : Text("먹었다!"),
+          ).toList(),
+          ...[
+            {
+              "direct": "top",
+              "coord": [0.0, -0.1],
+              "location": Alignment.topCenter,
+            },
+            {
+              "direct": "bottom",
+              "coord": [0.0, 0.1],
+              "location": Alignment.bottomCenter,
+            },
+            {
+              "direct": "left",
+              "coord": [-0.1, 0.0],
+              "location": Alignment.centerLeft,
+            },
+            {
+              "direct": "right",
+              "coord": [0.1, 0.0],
+              "location": Alignment.centerRight,
+            },
+          ]
+              .map(
+                (e) => Align(
+                  alignment: e['location'] as Alignment,
+                  child: GestureDetector(
+                    onTapDown: (_) => _startMove(e['coord'] as List<double>),
+                    onTapUp: (_) => _stopMove(),
+                    onTapCancel: () => _stopMove(),
+                    child: Image.asset(
+                      "assets/images/whale/button_${e['direct']}.png",
+                      width: 130,
                     ),
                   ),
-                )
-                .toList(),
+                ),
+              )
+              .toList(),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Container(
+              width: MediaQuery.of(context).size.width / 2 - 200,
+              height: 80,
+              padding: const EdgeInsets.all(20),
+              child: PercentBar({"count": _fishCount, "barColor": Color(0xFFFF835C) ,"imgUrl": "assets/images/whale/fish_pink.png"}),
+            ),
           ),
         ],
       ),

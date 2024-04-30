0e3f86ad1b4341ff767d936ffc45c81b37abcf05
subin22643 <139415947+subin22643@users.noreply.github.com>
2024-04-30 15:43:41 +0900

    Feat : 곰 집 소켓 연결 추가
    Change-Id: I9b3a40a7440b60f90059b4d4ba9a86c0ff35da76

--- front/ggomal/lib/screens/kids/bingo.dart
+++ front/ggomal/lib/screens/kids/bingo.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:ggomal/utils/navbar.dart';
-// import 'package:web_socket_channel/web_socket_channel.dart';
-// import 'package:web_socket_channel/status.dart' as status;
+
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/status.dart' as status;
 
 class BingoScreen extends StatefulWidget {
   const BingoScreen({super.key});
@@ -11,46 +12,38 @@ class BingoScreen extends StatefulWidget {
 }
 
 class _BingoScreenState extends State<BingoScreen> {
-  // final socketUrl = Uri.parse('wss://k10e206.p.ssafy.io/api/v1/room');
-  // final channel = WebSocketChannel.connect(Uri.parse('wss://k10e206.p.ssafy.io/api/v1/room'));
-  //
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   connectToWebSocket();
-  // }
-  //
-  // void connectToWebSocket() {
-  //   try {
-  //     channel.stream.listen((message) {
-  //       print("소켓 통신 성공~~~!: $message");
-  //       // channel.sink.add('소켓 통신~~~!!!');
-  //     }, onDone: () {
-  //       print('소켓 통신 성공 했다가 닫힐때 나오는 메시지임 ');
-  //     }, onError: (error) {
-  //       print('소켓 통신에 실패했습니다.');
-  //     });
-  //     print('WebSocket connection established.');
-  //   } catch (e) {
-  //     print('소켓 통신 에러날 때 나오는 메시지 $e');
-  //   }
-  // }
-  //
-  // @override
-  // void dispose() {
-  //   channel.sink.close(status.goingAway);
-  //   super.dispose();
-  // }
+  late final WebSocketChannel channel;
+  bool isConnected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    connectToWebSocket();
+  }
+
+  void connectToWebSocket() {
+    if (!isConnected) {
+      channel = WebSocketChannel.connect(Uri.parse('ws://k10e206.p.ssafy.io/api/v1/room'));
+      channel.stream.listen((message) {
+        print("소켓 통신 성공~~~!: $message");
+      }, onDone: () {
+        print('소켓 통신 하는중인듯?? ');
+      }, onError: (error) {
+        print('소켓 통신에 실패했습니다.');
+      });
+      print('WebSocket connection established.');
+      isConnected = true;
+    }
+  }
+
+  @override
+  void dispose() {
+    channel.sink.close(status.goingAway);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // channel.ready;
-    //
-    // channel.stream.listen((message) {
-    //   channel.sink.add('소켓 통신~~~!!!');
-    //   channel.sink.close(status.goingAway);
-    // });
-
     return Container(
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -70,9 +63,11 @@ class _BingoScreenState extends State<BingoScreen> {
                   flex: 1,
                 ),
                 Flexible(
+                  flex: 3,
                   child: Column(
                     children: [
                       Flexible(
+                        flex: 1,
                         child: Center(
                           child: Container(
                             width: 200,
@@ -99,14 +94,14 @@ class _BingoScreenState extends State<BingoScreen> {
                         ),
                       ),
                       Flexible(
-                        child: Container(color: Colors.red),
                         flex: 4,
+                        child: Container(color: Colors.red),
                       ),
                     ],
                   ),
-                  flex: 3,
                 ),
                 Flexible(
+                  flex: 3,
                   child: Column(children: [
                     Flexible(
                       child: Center(
@@ -135,11 +130,10 @@ class _BingoScreenState extends State<BingoScreen> {
                       ),
                     ),
                     Flexible(
-                      child: Container(color: Colors.yellow),
                       flex: 4,
+                      child: Container(color: Colors.yellow),
                     )
                   ]),
-                  flex: 3,
                 ),
                 Flexible(
                   child: Container(),

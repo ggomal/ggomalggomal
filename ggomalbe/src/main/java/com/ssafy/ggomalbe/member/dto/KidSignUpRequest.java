5a344eb9f597efd06768815846ecb78ccb5e266e
최진우 <chjnw0204@gmail.com>
2024-05-03 11:21:06 +0900

    Feat : NoticeResponse DateTime Formating
    Change-Id: Icf2fd318ee8a536983d7a79a1f0176e585a13537

---
+++ ggomalbe/src/main/java/com/ssafy/ggomalbe/member/dto/KidSignUpRequest.java
@@ -0,0 +1,2 @@
+package com.ssafy.ggomalbe.member.dto;public class KidSignUpRequest {
+}
--- ggomalbe/src/main/java/com/ssafy/ggomalbe/notice/dto/NoticeResponse.java
+++ ggomalbe/src/main/java/com/ssafy/ggomalbe/notice/dto/NoticeResponse.java
@@ -1,5 +1,6 @@
 package com.ssafy.ggomalbe.notice.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ssafy.ggomalbe.common.entity.NoticeEntity;
 import com.ssafy.ggomalbe.homework.dto.HomeworkResponse;
 import lombok.Builder;
@@ -19,8 +20,9 @@ public class NoticeResponse {
     private Long noticeId;
     private String teacherName;
     private String content;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private LocalDateTime date;
-    private List<HomeworkResponse> homeworks = new ArrayList<>();
+    private List<HomeworkResponse> homeworks;
 
     public NoticeResponse setHomeworks(List<HomeworkResponse> homeworks){
         this.homeworks = homeworks;

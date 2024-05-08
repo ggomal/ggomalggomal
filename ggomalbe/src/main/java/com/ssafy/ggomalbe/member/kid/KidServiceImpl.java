9279bc47aac484466bd31e3d2435c5c525c2cc40
최진우 <chjnw0204@gmail.com>
2024-05-08 16:59:44 +0900

    Feat : when add new kid, open situation '1'
    Change-Id: Idd958334b86e52f5fd388ffd8b4433e49d68584c

--- ggomalbe/src/main/java/com/ssafy/ggomalbe/member/kid/KidServiceImpl.java
+++ ggomalbe/src/main/java/com/ssafy/ggomalbe/member/kid/KidServiceImpl.java
@@ -2,9 +2,11 @@ package com.ssafy.ggomalbe.member.kid;
 
 import com.ssafy.ggomalbe.common.entity.KidEntity;
 import com.ssafy.ggomalbe.common.entity.MemberEntity;
+import com.ssafy.ggomalbe.common.entity.SituationKidEntity;
 import com.ssafy.ggomalbe.common.entity.TeacherKidEntity;
 import com.ssafy.ggomalbe.common.repository.KidRepository;
 import com.ssafy.ggomalbe.common.repository.MemberRepository;
+import com.ssafy.ggomalbe.common.repository.SituationKidRepository;
 import com.ssafy.ggomalbe.common.repository.TeacherKidRepository;
 import com.ssafy.ggomalbe.member.kid.dto.*;
 import lombok.RequiredArgsConstructor;
@@ -16,11 +18,12 @@ import reactor.core.publisher.Mono;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class KidServiceImpl implements KidService{
+public class KidServiceImpl implements KidService {
 
     private final MemberRepository memberRepository;
     private final KidRepository kidRepository;
     private final TeacherKidRepository teacherKidRepository;
+    private final SituationKidRepository situationKidRepository;
 
     @Override
     public Mono<KidSignUpResponse> insertKid(KidSignUpRequest request) {
@@ -33,18 +36,34 @@ public class KidServiceImpl implements KidService{
                 .doOnSuccess(this::saveTeacherKid)
                 .map(KidSignUpRequest::toKidSignUpResponse);
     }
-    public void saveKid(KidEntity kid){kidRepository.save(kid).subscribe();}
-    public void saveKid(KidSignUpRequest request){saveKid(request.toKidEntity());}
-    public void saveTeacherKid(TeacherKidEntity teacherKid){teacherKidRepository.save(teacherKid).subscribe();}
-    public void saveTeacherKid(KidSignUpRequest request){saveTeacherKid(request.toTeacherKidEntity());}
+
+    public void saveKid(KidEntity kid) {
+        kidRepository.save(kid).subscribe();
+        situationKidRepository.save(SituationKidEntity.builder()
+                .memberId(kid.getMemberId())
+                .situationId(1L)
+                .build()).subscribe();
+    }
+
+    public void saveKid(KidSignUpRequest request) {
+        saveKid(request.toKidEntity());
+    }
+
+    public void saveTeacherKid(TeacherKidEntity teacherKid) {
+        teacherKidRepository.save(teacherKid).subscribe();
+    }
+
+    public void saveTeacherKid(KidSignUpRequest request) {
+        saveTeacherKid(request.toTeacherKidEntity());
+    }
 
     @Override
     public Mono<MemberKidResponse> getKid(Long memberId) {
         return memberRepository.getMemberKid(memberId);
     }
 
-    public Flux<KidListResponse> getKidList(Long memberId){
-        System.out.println("get kids : i am teacher "+memberId);
+    public Flux<KidListResponse> getKidList(Long memberId) {
+        System.out.println("get kids : i am teacher " + memberId);
         return memberRepository.getKidList(memberId);
     }
 

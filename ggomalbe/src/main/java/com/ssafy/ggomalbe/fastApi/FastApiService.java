1e5bf6b436da1a3760ca7c05e287ca2224063269
최진우 <chjnw0204@gmail.com>
2024-05-09 20:01:13 +0900

    Feat : add fastApiService (temp until gpu server ON)
    Change-Id: Ifb7747174efd1ff5c93855af99cb69b52d497d33

---
+++ ggomalbe/src/main/java/com/ssafy/ggomalbe/fastApi/FastApiService.java
@@ -0,0 +1,62 @@
+package com.ssafy.ggomalbe.fastApi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class FastApiService {
+    private final WebClient webClient;
+
+    @Value("${fastApi.baseurl}")
+    private String fastApiUrl;
+
+    public FastApiService() {
+        this.webClient = WebClient.builder()
+                .baseUrl(fastApiUrl)
+                .build();
+    }
+
+    public Mono<Boolean> getTongue(FilePart frogGameImg) {
+        return this.webClient.post()
+                .uri("/fast/mf_recom/update")
+                .bodyValue(frogGameImg)
+                .retrieve()
+                .onStatus(httpStatus -> httpStatus.is4xxClientError() || httpStatus.is5xxServerError(),
+                        clientResponse -> Mono.error(new RuntimeException("API 호출 실패, 상태 코드: " + clientResponse.statusCode())))
+                .bodyToMono(Boolean.class);
+    }
+
+//    @Data
+//    @AllArgsConstructor
+//    @NoArgsConstructor
+//    static public class MfRecommendResponse {
+//
+//        @JsonProperty("news_seq")
+//        private Long newsSeq;
+//
+//        @JsonProperty("news_title")
+//        private String title;
+//
+//        @JsonProperty("news_similarity")
+//        private Double similarity;
+//    }
+//    @Data
+//    @AllArgsConstructor
+//    @NoArgsConstructor
+//    static public class CbfRecommendResponse {
+//
+//        private String news_id;
+//
+//        private Long news_seq;
+//
+//        private String news_title;
+//
+//        private Double news_similarity;
+//
+//        private Map<String, Double> news_keyword;
+//
+//    }
+}

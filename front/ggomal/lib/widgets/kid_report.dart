c771b126d08bc4ab5c6a0bf3d7882a45108c2c8c
지민 <81950388+jjm6604@users.noreply.github.com>
2024-05-10 17:42:46 +0900

    Feat : 학습 현황 구성
    Change-Id: Idf631b20b75eb55ba36810c0abd80f76acc8701b

--- front/ggomal/lib/widgets/kid_report.dart
+++ front/ggomal/lib/widgets/kid_report.dart
@@ -1,34 +1,144 @@
 import 'package:flutter/material.dart';
+import 'package:ggomal/constants.dart';
+import 'package:fl_chart/fl_chart.dart';
 
-class KidReport extends StatelessWidget {
+class KidReport extends StatefulWidget {
   const KidReport({super.key});
 
   @override
+  State<KidReport> createState() => _KidReportState();
+}
+
+class _KidReportState extends State<KidReport> {
+  Map reportData = {
+    "wordAccuracyMean": {
+      "ㅁ": 0.825148,
+      "ㄱ, ㅋ": 3.22583
+    },
+    "wordAccuracy": {
+      "ㅁ": [
+        {
+          "date": "2024-05-07",
+          "accuracyMean": 0.712574
+        },
+        {
+          "date": "2024-05-09",
+          "accuracyMean": 0.937722
+        }
+      ],
+      "ㄱ, ㅋ": [
+        {
+          "date": "2024-05-06",
+          "accuracyMean": 3.93264
+        },
+        {
+          "date": "2024-05-07",
+          "accuracyMean": 2.51902
+        }
+      ]
+    },
+    "whaleMaxTime": [
+      {
+        "date": "2024-05-09",
+        "meanMaxTime": 2.3
+      },
+      {
+        "date": "2024-05-10",
+        "meanMaxTime": 3.25
+      }
+    ],
+    "mostUsedWord": ["String", "String", "String"],
+    "chickAccuracy": [
+      {
+        "date": "2024-05-09",
+        "meanMaxTime": 2.3
+      },
+      {
+        "date": "2024-05-09",
+        "meanMaxTime": 2.3
+      }
+    ]
+  };
+
+
+  @override
   Widget build(BuildContext context) {
-    TextStyle baseText(double size, FontWeight weight) {
-      return TextStyle(
-        fontFamily: 'NanumS',
-        fontWeight: weight,
-        fontSize: size,
-        color: Colors.black,
-      );
-    }
-    return Scaffold(
-      body: Container(
-        width: 1000,
-          height: 600,
-          color: Colors.green[50],
-          child: Column(
-            children: [
-              Text('김이안 학생', style: baseText(50, FontWeight.bold)),
-              Text('단어 정확도', style: baseText(40, FontWeight.bold)),
-              Text('조음별 현황', style: baseText(30, FontWeight.bold)),
-              Container(height: 400 ,color: Colors.red,),
-              Text('일자별 현황', style: baseText(30, FontWeight.bold)),
-              Text('많이 연습한 단어', style: baseText(40, FontWeight.bold)),
-              Text('전체 게임 현황', style: baseText(40, FontWeight.bold)),
-            ],
-          )),
+
+    List<FlSpot> sleepdata = <FlSpot>[
+      FlSpot(0, 7),
+      FlSpot(1, 6),
+      FlSpot(2, 6),
+      FlSpot(3, 8),
+      FlSpot(4, 5),
+      FlSpot(5, 8),
+      FlSpot(6, 9),
+    ];
+
+
+    return Container(
+      color: Colors.white,
+      child: ListView(scrollDirection: Axis.vertical, children: [
+        Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              width: double.infinity,
+              height: 300,
+              color: Colors.grey[200],
+              child: Column(
+                children: [
+                  Text('단어 정확도(일자별)',
+                      style: nanumText(24, FontWeight.w900, Colors.black)),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 220,
+                    child: LineChart(
+                      LineChartData(),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              width: double.infinity,
+              height: 300,
+              color: Colors.grey[200],
+              child: Text('단어 정확도(평균)',
+                  style: nanumText(24, FontWeight.w900, Colors.black)),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              width: double.infinity,
+              height: 300,
+              color: Colors.grey[200],
+              child: Text('발성 연습(고래 게임)',
+                  style: nanumText(24, FontWeight.w900, Colors.black)),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              width: double.infinity,
+              height: 300,
+              color: Colors.grey[200],
+              child: Text('병아리 게임 발음 점수',
+                  style: nanumText(24, FontWeight.w900, Colors.black)),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              width: double.infinity,
+              height: 300,
+              color: Colors.grey[200],
+              child: Text('많이 사용한 단어',
+                  style: nanumText(24, FontWeight.w900, Colors.black)),
+            ),
+          ],
+        ),
+      ]),
     );
   }
 }

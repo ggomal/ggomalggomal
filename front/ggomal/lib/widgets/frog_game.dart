c3f61f02adcbf7b283e5af691f00038d3b19ff26
박서현 <mentos0601@naver.com>
2024-05-18 15:58:13 +0900

    Feat : 개구리 소리 넣기
    Change-Id: Ie13c80d339cfc0b9ecdaac36b0ba14064ef27dcd

--- front/ggomal/lib/widgets/frog_game.dart
+++ front/ggomal/lib/widgets/frog_game.dart
@@ -3,6 +3,7 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:gif/gif.dart';
 import '../services/frog_dio.dart';
 import '../utils/game_bosang_dialog.dart';
@@ -15,6 +16,7 @@ import 'package:flutter_vision/flutter_vision.dart';
 import 'percent_bar.dart';
 
 enum Options { none, imagev5, imagev8, imagev8seg, frame, tesseract, vision }
+
 late List<CameraDescription> cameras;
 
 class FrogTown extends StatefulWidget {
@@ -51,21 +53,18 @@ class _FrogTownState extends State<FrogTown> {
       body: Stack(
         children: [
           Positioned(
-            // bottom: 0,
+              // bottom: 0,
               child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                child: YoloVideo(
-                    vision: vision
-                ),
-              )),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: YoloVideo(vision: vision),
+          )),
           //FrogGame(key: frogGameStateKey),
         ],
       ),
     );
   }
 }
-
 
 class YoloVideo extends StatefulWidget {
   final FlutterVision vision;
@@ -80,24 +79,36 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
   late final GifController controller1, controller2, controller3;
   final AudioPlayer player = AudioPlayer();
   final AudioPlayer player_frog = AudioPlayer();
+  final AudioPlayer player_gif = AudioPlayer();
   // 개구리 게임
   late int _fishCount = 0;
 
   Random random = Random();
   final List<Map<String, dynamic>> _fishLocation = [
-    {"x": -0.7, "y": -0.7, "isVisible": true, "width" : 80},
-    {"x": -0.4, "y": -0.2, "isVisible": true, "width" : 95},
-    {"x": -0.3, "y": 0.2, "isVisible": true, "width" : 100},
-    {"x": -0.2, "y": -0.4, "isVisible": true, "width" : 120},
-    {"x": -0.1, "y": -0.6, "isVisible": true, "width" : 95},
-    {"x": 0.1, "y": -0.5, "isVisible": true, "width" : 105},
-    {"x": 0.4, "y": 0.2, "isVisible": true, "width" : 95},
-    {"x": 0.5, "y": -0.4, "isVisible": true, "width" : 110},
-    {"x": -0.6, "y": 0.3, "isVisible": true, "width" : 105},
-    {"x": 0.7, "y": -0.5, "isVisible": true, "width" : 102},
+    {"x": -0.7, "y": -0.7, "isVisible": true, "width": 80},
+    {"x": -0.4, "y": -0.2, "isVisible": true, "width": 95},
+    {"x": -0.3, "y": 0.2, "isVisible": true, "width": 100},
+    {"x": -0.2, "y": -0.4, "isVisible": true, "width": 120},
+    {"x": -0.1, "y": -0.6, "isVisible": true, "width": 95},
+    {"x": 0.1, "y": -0.5, "isVisible": true, "width": 105},
+    {"x": 0.4, "y": 0.2, "isVisible": true, "width": 95},
+    {"x": 0.5, "y": -0.4, "isVisible": true, "width": 110},
+    {"x": -0.6, "y": 0.3, "isVisible": true, "width": 105},
+    {"x": 0.7, "y": -0.5, "isVisible": true, "width": 102},
   ];
 
-  List<int> _availableIndexes = [0,1,2,3,4,5,6,7,8,9]; // 사용 가능한 인덱스 리스트
+  List<int> _availableIndexes = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9
+  ]; // 사용 가능한 인덱스 리스트
 
   bool _isStart = false;
 
@@ -109,16 +120,22 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
 
       player_frog.setVolume(1.0);
       player_frog.play(AssetSource('audio/frog/frog_eating.mp3'));
+      // player_gif.play(AssetSource('audio/frog/frog_taste.mp3'));
 
-      if(_fishCount == 10){
+      if (_fishCount == 10) {
         frogReword();
         Future.delayed(Duration(milliseconds: 500)).then((value) {
           stopDetection();
 
           showDialog(
             context: context,
-            builder: (BuildContext context) =>
-                GameContinueDialog2(continuePage: '/kids/frog', count: 2, onRestart: (){_restartGame();},),
+            builder: (BuildContext context) => GameContinueDialog2(
+              continuePage: '/kids/frog',
+              count: 2,
+              onRestart: () {
+                _restartGame();
+              },
+            ),
           );
         });
       }
@@ -128,10 +145,12 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
   void _restartGame() {
     setState(() {
       _fishCount = 0;
-      _fishLocation.forEach((fish) => fish['isVisible'] = true);  // 모든 물고기를 다시 보이게 설정
-      _availableIndexes = List.generate(_fishLocation.length, (index) => index);  // 인덱스 리스트 재설정
+      _fishLocation
+          .forEach((fish) => fish['isVisible'] = true); // 모든 물고기를 다시 보이게 설정
+      _availableIndexes =
+          List.generate(_fishLocation.length, (index) => index); // 인덱스 리스트 재설정
       _isStart = true;
-      startDetection();  // 감지 재시작
+      startDetection(); // 감지 재시작
     });
   }
 
@@ -150,20 +169,17 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
     controller2 = GifController(vsync: this);
     controller3 = GifController(vsync: this);
     init();
-    player.setVolume(0.1);
-    player.setReleaseMode(ReleaseMode.loop);
-    player.play(AssetSource('audio/frog/frog_game.mp3'));
+    // player.setVolume(0.1);
+    // player.setReleaseMode(ReleaseMode.loop);
+    // player.play(AssetSource('audio/frog/frog_game.mp3'));
   }
 
   init() async {
     cameras = await availableCameras();
     final frontCamera = cameras.firstWhere(
-          (camera) => camera.lensDirection == CameraLensDirection.front,
+      (camera) => camera.lensDirection == CameraLensDirection.front,
     );
-    controller = CameraController(
-        frontCamera,
-        ResolutionPreset.ultraHigh
-    );
+    controller = CameraController(frontCamera, ResolutionPreset.ultraHigh);
     controller.initialize().then((value) {
       loadYoloModel().then((value) async {
         setState(() {
@@ -177,7 +193,7 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
   @override
   void dispose() {
     controller.dispose();
-    player.stop();
+    // player.stop();
     super.dispose(); // 이 부분을 수정
   }
 
@@ -202,8 +218,8 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
         ),
         //...displayBoxesAroundRecognizedObjects(size),
         Positioned(
-          top : 220,
-          right : 780,
+          top: 220,
+          right: 780,
           // left: 0,
           // bottom: 75,
           // width: 250,
@@ -212,62 +228,84 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
             // width: 80,
             child: isDetecting
                 ? GestureDetector(
-              onTap: () {},
-              child: Container(
-                // decoration: BoxDecoration(
-                //     color: Color(0xFFFE5757),
-                //     borderRadius: BorderRadius.circular(10),
-                //     border: Border.all(color: Colors.black, width: 2)
-                // ),
-                child: Center(
-                  child: Column(
-                    children: [
-                      Gif(
-                        width: 600,
-                        controller: controller2,
-                        duration: const Duration(seconds: 20),
-                        autostart: Autostart.once,
-                        placeholder: (context) =>
-                        const Center(child: CircularProgressIndicator()),
-                        image: const AssetImage('assets/images/frog/frog_tongue_new.gif'),
+                    onTap: () {},
+                    child: Container(
+                      // decoration: BoxDecoration(
+                      //     color: Color(0xFFFE5757),
+                      //     borderRadius: BorderRadius.circular(10),
+                      //     border: Border.all(color: Colors.black, width: 2)
+                      // ),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Stack(
+                              children: [
+                                Positioned(
+                                  left: 200,
+                                  top: 50,
+                                  child: Image.asset(
+                                    "assets/images/frog/frog_tongue_text.png",
+                                    width: 200,
+                                  ),
+                                ),
+                                Positioned(
+                                    child: Gif(
+                                  width: 600,
+                                  controller: controller2,
+                                  duration: const Duration(seconds: 20),
+                                  autostart: Autostart.once,
+                                  placeholder: (context) => const Center(
+                                      child: CircularProgressIndicator()),
+                                  image: const AssetImage(
+                                      'assets/images/frog/frog_tongue_new.gif'),
+                                )),
+                              ],
+                            )
+                          ],
+                        ),
                       ),
-                      // Text("혀를 내밀어봐요 !", style: TextStyle(
-                      //     fontFamily: 'Maplestory',
-                      //     fontWeight: FontWeight.bold,
-                      //     fontSize: 30
-                      // ))
-                    ],
-                  ),
-                ),
-              ),
-            )
+                    ),
+                  )
                 : GestureDetector(
-              onTap: () async {
-                await startDetection();
-              },
-              child: Container(
-                // decoration: BoxDecoration(
-                //     color: Color(0xFFFE5757),
-                //     borderRadius: BorderRadius.circular(10),
-                //     border: Border.all(color: Colors.black, width: 2)
-                // ),
-                child: Center(
-                  child: Column(
-                    children: [
-                      Gif(
-                        width: 600,
-                        controller: controller2,
-                        duration: const Duration(seconds: 20),
-                        autostart: Autostart.once,
-                        placeholder: (context) =>
-                        const Center(child: CircularProgressIndicator()),
-                        image: const AssetImage('assets/images/frog/frog_eat.gif'),
-                      )
-                    ],
+                    onTap: () async {
+                      await startDetection();
+                    },
+                    child: Container(
+                      // decoration: BoxDecoration(
+                      //     color: Color(0xFFFE5757),
+                      //     borderRadius: BorderRadius.circular(10),
+                      //     border: Border.all(color: Colors.black, width: 2)
+                      // ),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Stack(
+                              children: [
+                                Positioned(
+                                    left: 200,
+                                    top: 40,
+                                    child: Image.asset(
+                                  "assets/images/frog/frog_eat_text.png",
+                                  width: 200,
+                                )),
+                                Positioned(
+                                    child: Gif(
+                                  width: 600,
+                                  controller: controller2,
+                                  duration: const Duration(seconds: 20),
+                                  autostart: Autostart.once,
+                                  placeholder: (context) => const Center(
+                                      child: CircularProgressIndicator()),
+                                  image: const AssetImage(
+                                      'assets/images/frog/frog_eat.gif'),
+                                ))
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
           ),
         ),
         ..._fishLocation.asMap().entries.map((entry) {
@@ -276,16 +314,20 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
 
           return fish['isVisible']
               ? Positioned(
-            left: (fish['x'] as double) * MediaQuery.of(context).size.width / 2 + MediaQuery.of(context).size.width / 2,
-            top: (fish['y'] as double) * MediaQuery.of(context).size.height / 2 + MediaQuery.of(context).size.height / 2,
-            child: GestureDetector(
-              onTap: () => _eatItem(index),
-              child: Image.asset(
-                  "assets/images/frog/strawberry_ss.png",
-                  width: fish['width'] / 1.0
-              ),
-            ),
-          )
+                  left: (fish['x'] as double) *
+                          MediaQuery.of(context).size.width /
+                          2 +
+                      MediaQuery.of(context).size.width / 2,
+                  top: (fish['y'] as double) *
+                          MediaQuery.of(context).size.height /
+                          2 +
+                      MediaQuery.of(context).size.height / 2,
+                  child: GestureDetector(
+                    onTap: () => _eatItem(index),
+                    child: Image.asset("assets/images/frog/strawberry_ss.png",
+                        width: fish['width'] / 1.0),
+                  ),
+                )
               : Container();
         }).toList(),
         Align(
@@ -304,32 +346,34 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
         _isStart
             ? SizedBox.shrink()
             : Container(
-          height: size.height * 0.1,
-          color: Colors.black.withOpacity(0.6),
-          child: Center(
-            child: GestureDetector(
-              onTap: () async {
-                await startDetection() ;
-                setState(() {
-                  _isStart = true;
-                });
-              },
-              child: Stack(
-                children: [
-                  Positioned(child: Image.asset(
-                    "assets/images/frog/frog_info.png",
-                  )),
-                  Positioned(
-                      top: size.height * 0.6, left: size.width * 0.3,
-                      child: Image.asset(
-                        "assets/images/frog/start_button.png",
-                        width: 300,
-                      ))
-                ],
+                height: size.height * 0.1,
+                color: Colors.black.withOpacity(0.6),
+                child: Center(
+                  child: GestureDetector(
+                    onTap: () async {
+                      await startDetection();
+                      setState(() {
+                        _isStart = true;
+                      });
+                    },
+                    child: Stack(
+                      children: [
+                        Positioned(
+                            child: Image.asset(
+                          "assets/images/frog/frog_info.png",
+                        )),
+                        Positioned(
+                            top: size.height * 0.6,
+                            left: size.width * 0.3,
+                            child: Image.asset(
+                              "assets/images/frog/start_button.png",
+                              width: 300,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ),
       ],
     );
   }
@@ -366,7 +410,8 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
         isDetecting = false;
       });
 
-      int randomIndex = _availableIndexes[random.nextInt(_availableIndexes.length)];
+      int randomIndex =
+          _availableIndexes[random.nextInt(_availableIndexes.length)];
       _eatItem(randomIndex);
 
       Future.delayed(Duration(milliseconds: 2500), () {
@@ -379,6 +424,7 @@ class _YoloVideoState extends State<YoloVideo> with TickerProviderStateMixin {
 
   Future<void> startDetection() async {
     print("#######################################");
+    player_gif.play(AssetSource('audio/frog/frog_tongue.mp3'));
     setState(() {
       isDetecting = true;
     });
